840ce127baaaaed664aeb3e07769bacea40f674f
Aamir Ansari <aamir@example.com>
2025-10-22 19:50:49 +0530

    updated command

--- render-build.sh
+++ render-build.sh
@@ -2,8 +2,8 @@
 set -o errexit
 
 # Install system dependencies
-apt-get update -y
-apt-get install -y espeak-ng libespeak-ng1
+sudo apt-get update -y
+sudo apt-get install -y espeak-ng libespeak-ng1
 
 # Upgrade pip (optional)
 pip install --upgrade pip
