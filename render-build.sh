15ddd22893ec689a6df0fd8c9d512d0946b2b627
Aamir Ansari <aamir@example.com>
2025-10-22 19:48:44 +0530

    Add espeak install for Render and updated requirements file

--- render-build.sh
+++ render-build.sh
@@ -2,7 +2,11 @@
 set -o errexit
 
 # Install system dependencies
-apt-get update && apt-get install -y espeak-ng
+apt-get update -y
+apt-get install -y espeak-ng libespeak-ng1
+
+# Upgrade pip (optional)
+pip install --upgrade pip
 
 # Then install Python deps
 pip install -r backend_requirements.txt
